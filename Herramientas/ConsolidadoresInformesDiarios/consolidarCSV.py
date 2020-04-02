604838f59e07095260a449a8c5019f7d10a5528a
Esteban <estebaniglesiasmanriquez@gmail.com>
2020-04-02 12:22:25 -0300

    ScriptConsolidar CSV y primer consolidado por Regiones

--- Herramientas/ConsolidadoresInformesDiarios/consolidarCSV.py
+++ Herramientas/ConsolidadoresInformesDiarios/consolidarCSV.py
@@ -5,11 +5,16 @@ import glob
 from datetime import date
 import numpy as np
 
-pathImport='../../informes_minsal/informes_diarios_CSV/'
+
+##Primero consolidamos los regionales
+
+pathImport='../../informes_minsal/informes_diarios_Region_CSV/'
 pathExport='../../'
 
 allfiles = [i for i in glob.glob((pathImport+'*.{}').format('csv'))]
 auxBool=True
+
+primero=True
 
 
 for i in allfiles:
@@ -23,13 +28,24 @@ for i in allfiles:
     #para simplemente luego tener una columna con la fecha
     
     
-    fecha['fecha']=date(*map(int,dateString.split('-')))
+    fecha['fecha']=dateString
+    #=date(*map(int,dateString.split('-')))
 
     #juntamos los dos dataframes
     df_con_fecha=pd.concat([fecha['fecha'],df],axis=1)
     
-    
-    with open("consolidado.txt",'a') as f:
-        df_con_fecha.to_csv(f,header=auxBool,sep=",",index=False)
-        f.close()
-    auxBool=False
+    if primero:
+        #Creamos el consolidado usando la fecha mas antigua
+        df_consolidado=df_con_fecha.copy()
+        
+        print('primero')
+    else:
+        #para todo el resto los unimos abajo
+        print('segundo')
+        df_consolidado=df_consolidado.append(df_con_fecha)
+        
+    primero=False
+
+### guardamos el archivo
+        
+df_consolidado.to_csv(pathExport+'COVID19_Chile_Region.CSV', index=False)
