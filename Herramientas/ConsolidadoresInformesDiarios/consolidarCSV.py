a95387712122a05ce612bea4f9f51d6bd93d3a41
Esteban <estebaniglesiasmanriquez@gmail.com>
2020-04-02 16:03:30 -0300

    Script actualizado

--- Herramientas/ConsolidadoresInformesDiarios/consolidarCSV.py
+++ Herramientas/ConsolidadoresInformesDiarios/consolidarCSV.py
@@ -49,3 +49,64 @@ for i in allfiles:
 ### guardamos el archivo
         
 df_consolidado.to_csv(pathExport+'COVID19_Chile_Region.CSV', index=False)
+
+
+
+
+
+
+##Ahora consolidamos los Comunales
+
+pathImport='../../informes_minsal/informes_diarios_Comuna_CSV/'
+pathExport='../../'
+
+allfiles = [i for i in glob.glob((pathImport+'*.{}').format('csv'))]
+auxBool=True
+
+primero=True
+
+
+for i in allfiles:
+
+    dateString=(i.replace(pathImport,'')[0:10])
+    df = pd.read_csv((pathImport+i),",")
+    #filas=df.shape[0]
+    
+    #primero hacemos una columna de puros zeros
+    fecha = pd.DataFrame(np.zeros((len(df), 1)))
+    #para simplemente luego tener una columna con la fecha
+    
+    
+    fecha['fecha']=dateString
+    #=date(*map(int,dateString.split('-')))
+
+    #juntamos los dos dataframes
+    df=pd.concat([fecha['fecha'],df],axis=1)
+    
+    
+    ### Los datos por Comuna tienen que ser arreglados.
+    #   Primero, a partir de la columna de tasa y la de población, hay que
+    #   reconstruir los datos de los casos (porque sólo informan cuando hay más
+    # de 4 casos)
+    
+    df['casos_totales']=df.casos_totales.replace('-',0)
+    df['casos_totales']=df.casos_totales.astype(int)
+    
+    ##Ahora corregimos los datos de los casos totales.
+    df['casos_totales']=(df.tasa*df.poblacion/100000).round(0).astype(int)
+    
+    if primero:
+        #Creamos el consolidado usando la fecha mas antigua
+        df_consolidado=df_con_fecha.copy()
+        
+        print('primero')
+    else:
+        #para todo el resto los unimos abajo
+        print('segundo')
+        df_consolidado=df_consolidado.append(df_con_fecha)
+        
+    primero=False
+
+### guardamos el archivo
+        
+df_consolidado.to_csv(pathExport+'COVID19_Chile_Comunas.CSV', index=False)
---
+++ Herramientas/Readme.md
@@ -0,0 +1,6 @@
+* /ArregladorCSVDiarios - Ya no es necesario usarlo. Era para arreglar los datos de los informes diarios por región, para cuando no daban casos nuevos ni fallecidos nuevos.
+
+* /ConsolidadoresInformesDiarios - Todos los días hay que correr este programa, recopila los informes diarios por Región y por Comuna.
+
+# Cosas por Hacer (ToDo):
+* Incorporar código de las comunas, para usar más fácilmente polígonos de comunas y hacer mapas.
