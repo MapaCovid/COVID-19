2a62f92eb79b1f21d1eb65fbf4cd2abfc326eed7
Esteban <estebaniglesiasmanriquez@gmail.com>
2020-04-08 13:25:39 -0300

    prueba

--- actualizacion/gitPullPush.py
+++ actualizacion/gitPullPush.py
@@ -8,12 +8,13 @@ Created on Wed Apr  8 13:07:33 2020
 from git import Repo
 from variables import pathRepo
 repo = Repo(pathRepo)  # if repo is CWD just do '.'
-
+mensajeCommit='creación script gitPullPush'
 def gitPull():
     repo.git.pull('origin', 'master')
-def gitPush():
+def gitPush(mensajeCommit):
     repo.git.add(update=True)        
-    repo.index.add(update=True)
+    repo.index.commit(mensajeCommit)
+    repo.git.push('origin', 'master')
 repo.index.commit('my commit description')
 
-origin.push()
+#origin.push()
