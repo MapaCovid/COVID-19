b2daff9e3df0c94dc899aad75b64ce3a9aaafb45
Esteban <estebaniglesiasmanriquez@gmail.com>
2020-04-17 20:04:50 -0300

    extraccion informes EPI

--- actualizacion/actualizarTodo.py
+++ actualizacion/actualizarTodo.py
@@ -7,17 +7,32 @@ Created on Mon Apr  6 00:52:21 2020
 """
 
 from extraerDatosRegiones import extraerDatosRegiones
-from consolidarCSV import consolidarCSV
+from consolidarCSV import consolidarCSVRegiones, consolidarCSVComunas
 from gitPullPush import gitPull, gitPush
+from descargadorInformesEPI import descargadorInformesEPI
+from extraerDatosInformeEPI import extraerDatosInformeEPI
 
-(actualizacionBool,fechaActualizacion)=extraerDatosRegiones()
-if actualizacionBool:
+(actualizacionBoolRegiones,fechaActualizacionRegiones)=extraerDatosRegiones()
+(actualizacionBoolComunas,fechaActualizacionComunas)=descargadorInformesEPI
+
+#Primero para las Regiones
+if actualizacionBoolRegiones:
     # la función extraerDatoRegiones devuelve True si extrajo algo
     # devuelve False si ya estábamos actualizados
-    print('Tenemos nuevos datos, vamos a consolidar.')
-    consolidarCSV()
+    print('Tenemos nuevos datos por Regiones, vamos a consolidar.')
+    consolidarCSVRegiones()
     gitPull
-    mensajeCommit='actualización automática '+fechaActualizacion
+    mensajeCommit='auto-update Regiones'+fechaActualizacionRegiones
+    gitPush(mensajeCommit)
+    
+if actualizacionBoolComunas:
+    print('Tenemos nuevos datos por Comunas')
+    
+    print('Primero vamos a extraer los datos de los PDFs')
+    extraerDatosInformeEPI()
+    consolidarCSVComunas()
+    git pull
+    mensajeCommit='auto-update Informe EPI'+fechaActualizacionComunas
     gitPush(mensajeCommit)
     
     
