712e684ba889f929d97113ed6384e562f59b2295
Esteban <estebaniglesiasmanriquez@gmail.com>
2020-04-17 19:33:11 -0300

    Agrego informe EPI y función descargadorInformesEPI.py

---
+++ actualizacion/descargadorInformesEPI.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 17 19:29:44 2020
+
+@author: esteban
+"""
+
+def descargadorInformesEPI():
+    #Esta función debería descargar los informes EPI disponibles en
+    # https://www.gob.cl/coronavirus/cifrasoficiales/
+    # Y dejarlos en /fuentes/informes_departamento_epidimiologia/
+    # con el formato "2020-04-15-INFORME_EPI_COVID19.pdf"
+    
+    # (Para eso, primero tiene que ver si hay un nuevo informe)
+    # Si descarga un nuevo informe, debería devolver (True, "YYY-MM-DD")
+    
+    
+    # Por ahora, lo estamos haciendo a mano. Necesitamos programar aquí!
+    return (True,"2020-04-15");
